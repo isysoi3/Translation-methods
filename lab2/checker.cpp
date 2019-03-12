b65107d95535085c4439baea61d8de70ab68836b
Ilya Sysoi <ilya.sysoi@gmail.com>
2019-03-12 19:15:49 +0300

    lab2: fixed algoritm

--- lab2/checker.cpp
+++ lab2/checker.cpp
@@ -9,33 +9,32 @@ int B() {
     c = getchar();
     if (c == 'a')
         return 1;
-    else if (B()) {
-        //где то здесь
-        return 1;
-        c = getchar();
-        if(c == '\n')
-            return 2;
-        if(c == 'a') {
+    else if (c == 'b') {
+        if (B()) {
             c = getchar();
             if (c == 'b')
                 return 1;
-        } else if (B())
-            return 1;
+        }
     }
     return 0;
 }
 
 int R() {
     char c;
-    int rez = B();
-    if (rez == 2)
-        return 1;
-    if (rez == 1) {
-        c = getchar();
-        if(c != 'b')
-            return 0;
-        if (A())
+    c = getchar();
+    if (c == 'b') {
+        char c1 = getchar();
+        if (c1 == '\n')
             return 1;
+        else
+            ungetc(c1, stdin);
+    }
+    ungetc(c, stdin);
+    if (B()) {
+        c = getchar();
+        if (c == 'b') {
+            return A();
+        }
     }
     return 0;
 }
@@ -55,7 +54,6 @@ int main() {
         printf("success!");
     else
         printf("ERROR !");
-
 }
 
 //abbabbbab
